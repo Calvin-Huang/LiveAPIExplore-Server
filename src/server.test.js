6d12ebaf204621fab43e5979691fb2b41b94d0e0
Calvin Huang <calvin.peak@capslock.tw>
2016-12-08 04:43:17 +0900

    Add auth testing.

---
+++ src/server.test.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch';
+import { expect } from 'chai';
+
+import runServer from '../tools/runServer';
+
+describe('server', () => {
+  let serve = null;
+
+  before(async () => {
+
+    // Wait for server up
+    serve = await runServer();
+  });
+
+  after(() => {
+    serve.kill('SIGTERM');
+  });
+
+  it('should prevent response context without authorization', async () => {
+    const response = await fetch('http://localhost:3000/');
+
+    expect(response.status).to.eq(401);
+  });
+});
