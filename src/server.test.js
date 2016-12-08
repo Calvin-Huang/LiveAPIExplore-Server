01f55f1ec9b9c1e6c872bf5e18ee5f512a101b63
Calvin Huang <calvin.peak@capslock.tw>
2016-12-08 19:39:01 +0900

    Add test case for ensure express will protect resource when request without credential or wrong access level.

--- src/server.test.js
+++ src/server.test.js
@@ -1,30 +1,122 @@
 import fetch from 'node-fetch';
 import { expect } from 'chai';
 
+import { Admin, User } from './data/models';
+import passwordHash from 'password-hash';
+
 import runServer from '../tools/runServer';
 
+import util from 'util';
+
 describe('server', () => {
-  let serve = null;
+  context('will protect resources', () => {
+    let serve = null;
 
-  before(async () => {
+    before(async () => {
 
-    // Wait for server up
-    serve = await runServer();
-  });
+      // Wait for server up
+      serve = await runServer();
 
-  after(() => {
-    serve.kill('SIGTERM');
-  });
+      // Prepare Admin & User's fixture
+      Admin.insertOrUpdate({ username: 'calvin.admin@capslock.tw', password: passwordHash.generate('12364362') });
+    });
 
-  it('should prevent response context without authorization', async () => {
-    const response = await fetch('http://localhost:3000/');
+    after(() => {
+      serve.kill('SIGTERM');
 
-    expect(response.status).to.eq(401);
-  });
+      Admin.destroy({ where: { username: 'calvin.admin@capslock.tw' } });
+      User.destroy({ where: { email: 'calvin.user@capslock.tw' } });
+    });
 
-  it('should allow visit to page login', async () => {
-    const response = await fetch('http://localhost:3000/login');
+    it('should prevent response context without authorization', async () => {
+      const response = await fetch('http://localhost:3000/');
 
-    expect(response.status).to.eq(200);
+      expect(response.status).to.eq(401);
+    });
+
+    it('should allow to visit the login page', async () => {
+      const response = await fetch('http://localhost:3000/login');
+
+      expect(response.status).to.eq(200);
+    });
+
+    it('should allow to visit the graphql resource', async () => {
+      const response = await fetch('http://localhost:3000/graphql', { method: 'POST' });
+
+      expect(response.status).to.eq(400);
+    })
+
+    it('should allow visit to root path with admin credential', async () => {
+      const query = `
+        mutation ($username: String!, $password: String!){
+          login(username: $username, password: $password) {
+            jwtToken
+          }
+        }
+      `;
+      const variables = {
+        username: 'calvin.admin@capslock.tw',
+        password: '12364362',
+      }
+
+      const login = await fetch('http://localhost:3000/graphql', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: query, variables: variables }),
+      });
+
+      const jwtToken = (await login.json()).data.login.jwtToken;
+
+      const response = await fetch('http://localhost:3000/', {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+
+      expect(response.status).to.eq(200);
+    });
+
+    it('should prevent visit root page with user credential', async () => {
+      const query = `
+        mutation ($id: ID!, $email:String, $claim: InputUserClaim, $profile: InputUserProfile) {
+          registerOrLogin(id: $id, email: $email, claim: $claim, profile: $profile) {
+            jwtToken
+          }
+        }
+      `;
+      const variables = {
+        id: '0c099de0-b2f4-11e6-8c63-a7513db19171',
+        email: 'calvin.user@capslock.tw',
+        claim: {
+          accessToken: 'test',
+        },
+        profile: {
+          displayName: 'Calvin Huang',
+          gender: 'male',
+        }
+      };
+
+      const login = await fetch('http://localhost:3000/graphql', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: query, variables: variables }),
+      });
+
+      const jwtToken = (await login.json()).data.registerOrLogin.jwtToken;
+
+      const response = await fetch('http://localhost:3000/', {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+
+      expect(response.status).to.eq(401);
+    });
   });
 });
