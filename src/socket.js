51708ba549bfa3b4e711eb1d914772625bee069f
Calvin Huang <calvin.peak@capslock.tw>
2016-12-11 02:02:52 +0900

    Complete socket server.

---
+++ src/socket.js
@@ -0,0 +1,40 @@
+import SocketIO from 'socket.io';
+import { createClient } from 'redis';
+
+const io = SocketIO();
+
+io.use((socket, next) => {
+  return next();
+});
+
+io.of('/live-chatroom')
+  .use((socket, next) => {
+    return next();
+  })
+  .on('connection', (socket) => {
+    const redis = createClient();
+
+    socket.on('subscribe', (id) => {
+      socket.join(`live:${id}:comments`);
+      redis.subscribe(`live:${id}:comments:latest`);
+
+      socket.emit('subscribed');
+    });
+
+    redis.on('message', (channel, message) => {
+      io.of('/live-chatroom').in(channel.replace(':latest', '')).emit('comment', message);
+    });
+
+    socket.on('unsubscribe', (id) => {
+      redis.unsubscribe(`live:${id}:comments:latest`);
+      socket.leave(id);
+
+      socket.emit('unsubscribed');
+    });
+
+    socket.on('disconnect', () => {
+      redis.quit();
+    })
+  });
+
+export default io;
