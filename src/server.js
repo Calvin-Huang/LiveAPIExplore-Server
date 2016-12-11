3e39403cbde50855f3e2788be88982fbb77ee791
Calvin Huang <calvin.peak@capslock.tw>
2016-12-11 16:50:46 +0900

    Add allowed path.

--- src/server.js
+++ src/server.js
@@ -78,7 +78,7 @@ app.use(
     },
   }),
   (req, res, next) => {
-    const allowedPaths = ['/login', '/graphql', '/auth/facebook', '/auth/facebook/callback'];
+    const allowedPaths = ['/login', '/graphql', '/auth/facebook', '/auth/facebook/callback', '/fb-subscribe'];
 
     if (allowedPaths.indexOf(req.path) > -1) {
       return next();
