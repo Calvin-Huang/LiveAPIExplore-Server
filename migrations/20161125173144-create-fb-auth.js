d7c83cc038fc594f40ab55a15ad5ade038b14919
Calvin Huang <calvin.peak@capslock.tw>
2016-12-11 14:36:55 +0900

    Fix sequlize migration, using singular.

--- migrations/20161125173144-create-fb-auth.js
+++ migrations/20161125173144-create-fb-auth.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('FBAuths', {
+    return queryInterface.createTable('FBAuth', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('FBAuths');
+    return queryInterface.dropTable('FBAuth');
   }
 };
--- seeders/20161204173948-default-admin.js
+++ seeders/20161204173948-default-admin.js
@@ -6,7 +6,10 @@ var moment = require('moment');
 module.exports = {
   up: function (queryInterface, Sequelize) {
     var date = moment().format('YYYY-MM-DD HH:mm:ss');
-    return queryInterface.bulkInsert('Admins', [{ username: process.env.SEED_ADMIN_USERNAME, password: passwordHash.generate(process.env.SEED_ADMIN_PASSWORD), createdAt: date, updatedAt: date }], {});
+
+    queryInterface.bulkDelete('Admin', [{ username: process.env.SEED_ADMIN_USERNAME }]);
+
+    return queryInterface.bulkInsert('Admin', [{ username: process.env.SEED_ADMIN_USERNAME, password: passwordHash.generate(process.env.SEED_ADMIN_PASSWORD), createdAt: date, updatedAt: date }], {});
   },
 
   down: function (queryInterface, Sequelize) {
