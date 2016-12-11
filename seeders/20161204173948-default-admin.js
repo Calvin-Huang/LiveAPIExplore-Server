926b17013b68548ee67846c3e2b45081a937f8cc
Calvin Huang <calvin.peak@capslock.tw>
2016-12-11 13:44:44 +0900

    Update moment add fix seeder for mysql.

--- seeders/20161204173948-default-admin.js
+++ seeders/20161204173948-default-admin.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var passwordHash = require('password-hash');
+var moment = require('moment');
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Admins', [{ username: process.env.SEED_ADMIN_USERNAME, password: passwordHash.generate(process.env.SEED_ADMIN_PASSWORD), createdAt: Date(), updatedAt: Date() }], {});
+    var date = moment().format('YYYY-MM-DD HH:mm:ss');
+    return queryInterface.bulkInsert('Admins', [{ username: process.env.SEED_ADMIN_USERNAME, password: passwordHash.generate(process.env.SEED_ADMIN_PASSWORD), createdAt: date, updatedAt: date }], {});
   },
 
   down: function (queryInterface, Sequelize) {
