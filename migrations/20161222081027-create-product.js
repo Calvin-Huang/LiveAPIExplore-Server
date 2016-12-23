5a8ce9819d59fc6eb3676ed6be4d895c224b43a3
Calvin Huang <calvin.peak@capslock.tw>
2016-12-23 10:44:51 +0900

    Fix column type.

--- migrations/20161222081027-create-product.js
+++ migrations/20161222081027-create-product.js
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       otherDescription: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
